1bf17ae54f0644ea99df5831448735413647f61b
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-07-30 07:41:48 +0100

    Update datas.blade.php

--- resources/views/partials/expenses/datas.blade.php
+++ resources/views/partials/expenses/datas.blade.php
@@ -23,7 +23,7 @@
                                         <!-- Outflow date -->
                                         <div class="col-sm-6">
                                             <label for="outflow_date" class="form-label fw-bold">Date/Heure de sortie</label>
-                                            <input type="datetime" name="outflow_date" class="form-control" id="outflow_date" value="{{ !empty($selected_expense->outflow_date) ? $selected_expense->outflow_date->format('d/m/Y H:i') : null }}">
+                                            <input type="datetime" name="outflow_date" class="form-control" id="outflow_date" value="{{ !empty($selected_expense->outflow_date) ? Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $selected_expense->outflow_date)->format('d/m/Y H:i') : null }}">
                                         </div>
 {{-- 
                                         <!-- Add order -->
